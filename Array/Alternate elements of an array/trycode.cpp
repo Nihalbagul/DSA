69982e8ee8ddac0002c8edf0bb341bbadab90221
Nihalbagul <nihalbagul98@gmail.com>
2025-04-08 11:01:18 +0530

    trying

---
+++ Array/Alternate elements of an array/trycode.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 10;
+    int arr[] = {10, 20, 30, 40, 50};
+
+    for(int i = 0; i < 5; i += 2)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
